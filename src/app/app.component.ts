b7f8abe8714d6614a0d06ef1c63117786376b8a4
Sanvi Rahman <SanviRahman>
2025-07-27 18:19:08 +0600

    Completed Reactive Form

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserComponent } from "./user/user.component";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,51 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule,NgIf],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  newgroup: FormGroup;
+
+
+  constructor(private fm: FormBuilder) {
+    this.newgroup = this.fm.group({
+      fname: ['', [Validators.required]],
+      lname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  get fname() {
+    return this.newgroup.get('fname');
+  }
+  get lname() {
+    return this.newgroup.get('lname');
+  }
+
+  get email() {
+    return this.newgroup.get('email');
+  }
+
+  get password() {
+    return this.newgroup.get('password');
+  }
+
+
+  onDisplay(){
+    if(this.newgroup.valid){
+      console.log(this.newgroup.value);
+    }
+    else{
+      console.log('Form is invalid');
+    }
+  }
+
+}
